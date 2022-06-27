b049a55474d02bdd79c3b15d38f946cb49500ed5
rapishard <alessandrorapisarda1981@gmail.com>
2022-06-27 16:28:03 +0200

    Material Design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./App.css";
+/* import { MDCTextField } from '@material/textfield';
+import { MDCCheckbox } from '@material/checkbox';
+import { MDCRipple } from '@material/ripple';
+import { MDCFormField } from '@material/form-field'; */
+
 
 function App() {
     return {
